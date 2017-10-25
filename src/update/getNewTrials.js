396cb34c3d423d125eeee77e169fea550c494864
bfarmilo <bill@farmilostrategy.com>
2017-10-25 12:27:29 -0400

    refactor to automatically scan through all returned records

--- src/update/getNewTrials.js
+++ src/update/getNewTrials.js
@@ -1,28 +1,74 @@
 //const fse = require('fse');
 const fetch = require('node-fetch');
-
+const { flatten } = require('../entities/helpers');
 
 const ptabUrl = 'https://ptabdata.uspto.gov/ptab-api';
 const header = {credentials: 'include'};
+const MAX_RETURN = 100;
 
-const getItems = (offset) => {
-  return fetch(`${ptabUrl}/trials?limit=100&sort=lastModifiedDatetime&lastModifiedDatetimeFrom=2017-07-01&offset=${offset}`, header)
-    .then(response => response.json())
-    .then(result => {
-      offset = (result.metadata.count <= offset + 100) ? offset + 100 : result.metadata.count;
-      return Promise.resolve({ offset, max: result.metadata.count, data: result.results });
-    })
-    .catch(err => Promise.reject(err));
+/**
+ * checkOffset returns the next offset, or maxCount if at the end of the collection
+ * 
+ * @param {Number} currentOffset the offset used for the last call
+ * @param {Number} maxCount the maximum number of records
+ * @returns {Number} the next offset 
+ * @private
+ */
+const checkOffset = (currentOffset, maxCount) => {
+   return (maxCount <= currentOffset + MAX_RETURN) ? maxCount : currentOffset + MAX_RETURN;
 }
 
+const getAllData = (query, header) => {
+  return new Promise((resolve, reject) => {
+    let returnArray = [];
+    let offset = 0;
+    const recurse = () => {
+      return fetch(`${query}&offset=${offset}`, header)
+      .then(response => response.json())
+      .then(jsonResult => {
+        returnArray.push(jsonResult.results);
+        offset += MAX_RETURN;
+        if (checkOffset(jsonResult.metadata.offset, jsonResult.metadata.count) !== jsonResult.metadata.count) {
+          recurse();
+        } else {
+          resolve({ 
+            max: jsonResult.metadata.count,
+            data: flatten(returnArray) 
+          })
+        }
+      })
+      .catch(err => reject(err));
+    }
+    recurse();
+  })
+}
+
+/**
+ * getItems hits the PTAB 'trials' collection
+ * 
+ * @returns {Promise} a promise that resolves to {max:number, data:Array[string]}
+ */
+const getItems = () => {
+  return getAllData(`${ptabUrl}/trials?limit=100&sort=lastModifiedDatetime&lastModifiedDatetimeFrom=2017-07-01`, header)
+  .then(result => Promise.resolve(result))
+  .catch(err => Promise.reject(err))
+}
+
+/**
+ * getBoardDocuments checks the PTAB 'documents' collection where the filingParty=board
+ * 
+ * @returns {Promise} a promise that resolves to {offset:number, max:number, data:Array[string]}
+ * where the returned offset is the next offset to use, or max if there are no more records
+ */
 const getBoardDocuments = (offset) => {
-  return fetch(`${ptabUrl}/documents?filingDatetimeFrom=2017-07-01&filingParty=board&offset=${offset}
-`, header)
-    .then(res => res.json())
+  return getAllData(`${ptabUrl}/documents?filingDatetimeFrom=2017-07-01&filingParty=board`, header)
     .then(result => Promise.resolve(result))
     .catch(err => Promise.reject(err));
 }
 
+/** module exports
+ * @public
+ */
 module.exports = {
   getItems,
   getBoardDocuments
--- src/update/getNewTrials.test.js
+++ src/update/getNewTrials.test.js
@@ -3,16 +3,17 @@ const { getItems, getBoardDocuments } = require('./getNewTrials.js');
 // get around certificate error with the PTAB API
 require('ssl-root-cas/latest').inject().addFile(__dirname + '../../../config/intermediate.crt');
 
- getItems(0)
+
+getItems()
   .then(result => {
-    console.log('returned offset %d', result.offset);
-    console.log('returned total count %d', result.count);
+    console.log('returned total count %d', result.max);
     console.log('returned cases\n%j', result.data.map(item => item.trialNumber))
     return;
   })
-  .then(() => getBoardDocuments(0))
+  .then(() => getBoardDocuments())
   .then(result => {
-    console.log(new Set(result.results.map(item => item.type)));
-    console.log(result.results.filter(item => item.type === 'Institution Decision').map(item => item.title))
+    console.log('returned total count %d', result.max);
+    console.log(new Set(result.data.map(item => item.type)));
+    //console.log(result.data.filter(item => item.type === 'Institution Decision').map(item => item.title))
   })
   .catch(err => console.error(err));
